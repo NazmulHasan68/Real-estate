d228161fe8bc6e223fb529996638f0a8e3c4348a
NazmulHasan3580 <n47712338@gmail.com>
2024-11-10 01:54:17 +0600

    conected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+try {
+    mongoose.connect(process.env.MONGO)
+    console.log("Database is connected successfully!");
+} catch (error) {
+    console.log("Database is not connected");
+}
+
 
 const app = express()
 app.listen(3000, ()=>{
