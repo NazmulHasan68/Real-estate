4ef762ce27e7e722c021fda69bc5aabf5652f025
NazmulHasan3580 <n47712338@gmail.com>
2024-11-14 12:57:56 +0600

    compleate sign page functionality

--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -1,21 +1,81 @@
-import {Link} from 'react-router-dom'
+import { useState } from "react";
+import { Link , useNavigate} from "react-router-dom";
 function Signin() {
+  const [formData, setFormData] = useState({})
+  const [error , setError] = useState(null)
+  const [Loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handlechange = (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value
+    })
+  }
+ 
+  const handleSumite = async(e) =>{
+    e.preventDefault()
+    try {
+      setLoading(true)
+    const res = await fetch('/api/auth/signin',{
+      method : "POST",
+      headers : {
+        'Content-type': 'application/json'
+      },
+      body : JSON.stringify(formData)
+    })
+    const data = await res.json()
+    if(data.success === false){
+      setError(data.message)
+      setLoading(false)
+      return
+    }
+    setLoading(false)
+    setError(null)
+    navigate('/')
+
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+    setLoading(false)
+  }
+  
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
-        <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email"/>
-        <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password"/>
-        <button className="bg-slate-600 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+      <h1 className="text-3xl text-center font-semibold my-7 text-slate-600">
+        Sign in
+      </h1>
+      <form onSubmit={handleSumite} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handlechange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handlechange}
+        />
+        <button disabled={Loading} className="bg-slate-600 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {Loading? 'Loading...' : 'Sign in'}
+        </button>
       </form>
-      <div className='flex gap-2 mt-4'>
-        <p>Have and account?</p>
-        <Link to={'/sign-up'}>
-          <span className='text-blue-700'>Sign up</span>
+      <div className="flex gap-2 mt-4">
+        <p>Dont haven account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
+      {
+        error && <p>{error}</p>
+      }
     </div>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,6 +28,7 @@ export const signincontroller = async (req, res, next) => {
 
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const {password:pass, ...rest} = validUser._doc
+        
         res.cookie('access_token', token, { httpOnly: true })
            .status(200)
            .json({ success: true, user: rest });
