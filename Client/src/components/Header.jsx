83e3c319268d05d0ed3dec9b780a469e1cc571f0
NazmulHasan3580 <n47712338@gmail.com>
2024-11-14 15:52:45 +0600

    Update header and make profile private

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import logo from "../assets/logo.png";
 import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+
 export default function Header() {
+    const { currentUser} = useSelector((state)=>state.user)
   return (
     <header className="bg-sky-200  p-2 sm:px-4 sm:py-2 "> 
         <div className="max-w-6xl mx-auto flex justify-between items-center">
@@ -16,7 +19,7 @@ export default function Header() {
                 />
                 <FaSearch className="text-sky-300" />
             </form>
-            <ul className="flex gap-4">
+            <ul className="flex gap-4 items-center">
                 <Link to={'/'}>
                     <li className="hidden sm:inline text-sky-700 hover:text-sky-800 cursor-pointer hover:font-medium transition-all duration-300">
                         Home
@@ -27,11 +30,20 @@ export default function Header() {
                         About
                     </li>
                 </Link>
-                <Link to={'/sign-in'}>
-                    <li className=" text-sky-700 hover:text-sky-800 cursor-pointer hover:font-medium transition-all duration-300">
-                        Sign in
-                    </li>
-                </Link>
+                {
+                    currentUser ? (
+                        <Link to={'/profile'}>
+                            <img src={currentUser.avatar} alt="profile image" className="h-8 w-8 object-cover rounded-full"/>
+                        </Link>
+                    ):(
+                        <Link to={'/sign-in'}>
+                            <li className=" text-sky-700 hover:text-sky-800 cursor-pointer hover:font-medium transition-all duration-300">
+                                Sign in
+                            </li>
+                        </Link>
+                    )
+                }
+                
         </ul>
         </div> 
     </header>
