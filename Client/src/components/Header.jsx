3718ce5207d9444eb08aea9d2ce601651e59c2e0
NazmulHasan3580 <n47712338@gmail.com>
2024-11-16 16:41:56 +0600

    update header search form functionality

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,24 +1,53 @@
 import logo from "../assets/logo.png";
 import { FaSearch } from "react-icons/fa";
-import { Link } from 'react-router-dom'
+import { Link , useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import { useEffect, useState } from "react";
 
 export default function Header() {
     const { currentUser} = useSelector((state)=>state.user)
+    const [searchTerm , setSearchTerm] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const urlParams =new URLSearchParams(window.location.search)
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString()
+        navigate(`/search?${searchQuery}`)
+
+    }
+
+    useEffect(()=>{
+        const urlParams = new URLSearchParams(location.search)
+        const searchTermFormUrl = urlParams.get('searchTerm');
+        if(searchTermFormUrl){
+            setSearchTerm(searchTermFormUrl)
+        }
+    },[location.search])
+
   return (
     <header className="bg-sky-200  p-2 sm:px-4 sm:py-2 "> 
         <div className="max-w-6xl mx-auto flex justify-between items-center">
             <Link to={'/'}>
                 <img src={logo} alt="logo" className="h-8 sm:h-10" />
             </Link>
-            <form className="bg-sky-100 p-2 rounded-lg flex items-center">
+
+
+            <form onSubmit={handleSubmit} className="bg-sky-100 p-2 rounded-lg flex items-center">
                 <input
-                type="text"
-                placeholder="Search..."
-                className="bg-transparent focus:outline-none w-28 sm:w-64"
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(e)=> setSearchTerm(e.target.value)}
+                    value={searchTerm}
+                    className="bg-transparent focus:outline-none text-slate-700 w-28 sm:w-64"
                 />
-                <FaSearch className="text-sky-300" />
+                <button type="sumit">
+                    <FaSearch className="text-sky-300" />
+                </button>
             </form>
+
+
             <ul className="flex gap-4 items-center">
                 <Link to={'/'}>
                     <li className="hidden sm:inline text-sky-700 hover:text-sky-800 cursor-pointer hover:font-medium transition-all duration-300">
